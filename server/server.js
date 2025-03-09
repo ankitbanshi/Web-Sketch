41b26036cab369c1beb6f1e226f61edf2ecd1b74
ankitbanshi <banshiwalankit448@gmail.com>
2025-03-09 15:07:10 +0530

    repo commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app=express();
+const server=require("http").createServer(app);
+const {Server} =require("socket.io");
+
+app.get("/",(req,res)=>{
+  res.send("our websketch app")
+})
+
+io.on("connection",(socket)=>{
+  console.log("user connected")
+})
+
+const port =process.env.PORT||8000;
+server.listen(port,()=>console.log(`server is running on http://localhost:${port}`))

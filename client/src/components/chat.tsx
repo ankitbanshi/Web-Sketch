abfea433aa307970ddc98400a5f912caaf35d4c9
ankitbanshi <banshiwalankit448@gmail.com>
2025-03-27 00:00:33 +0530

    new chat  button

--- client/src/components/chat.tsx
+++ client/src/components/chat.tsx
@@ -4,7 +4,7 @@ const ChatBox=()=>{
 
 
   return (
-    <button className="w-full py-2 bg-blue-800 text-white hover:bg-blue-400 p-5 mb-20 ">Chat</button>
+    <button className="w-full py-2 bg-blue-800  hover:bg-blue-400 p-5 mb-20 ">Chat</button>
 
   )
 }

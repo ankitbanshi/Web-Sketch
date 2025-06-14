0d0b104fceaab56ae7ec0df30c29439709ca9831
ankitbanshi <banshiwalankit448@gmail.com>
2025-06-15 00:19:49 +0530

    implement a working chatboard in serverside

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-const { userJoin, getUsers, userLeave } = require("./user");
+const { userJoin, getUsers, userLeave,getCurrentUser } = require("./user");
 
 const app = express();
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server);
+// serve on port
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -18,9 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("server");
-});
 
 // socket.io
 let imageUrl, userRoom;
@@ -37,31 +36,50 @@ io.on("connection", (socket) => {
     socket.broadcast.to(user.room).emit("message", {
       message: `${user.username} has joined`,
     });
+    
 
     io.to(user.room).emit("users", roomUsers);
     io.to(user.room).emit("canvasImage", imageUrl);
-  });
-
-  socket.on("drawing", (data) => {
-    imageUrl = data;
-    socket.broadcast.to(userRoom).emit("canvasImage", imageUrl);
-  });
-
-  socket.on("disconnect", () => {
-    const userLeaves = userLeave(socket.id);
-    const roomUsers = getUsers(userRoom);
-
-    if (userLeaves) {
-      io.to(userLeaves.room).emit("message", {
-        message: `${userLeaves.username} left the chat`,
-      });
-      io.to(userLeaves.room).emit("users", roomUsers);
-    }
-  });
+  }
+);
+  
+ socket.on("sendMessage", (message) => {
+  const user = getCurrentUser(socket.id);
+  console.log("Received message from:", user, "Message:", message); // <--- THIS LOG
+  if (user) {
+    io.to(user.room).emit("chatMessage", {
+      user: user.username,
+      text: message,
+      time: new Date().toISOString()
+    });
+  }
 });
 
-// serve on port
-const PORT = process.env.PORT || 5000;
+  socket.on("drawing", (data) => {
+  const user = getCurrentUser(socket.id);
+  if (user) {
+    imageUrl = data;
+    socket.broadcast.to(user.room).emit("canvasImage", imageUrl);
+  }
+});
+
+  socket.on("disconnect", () => {
+  const userLeaves = userLeave(socket.id);
+  if (userLeaves) {
+    const roomUsers = getUsers(userLeaves.room);
+    io.to(userLeaves.room).emit("message", {
+      message: `${userLeaves.username} left the chat`,
+    });
+    io.to(userLeaves.room).emit("users", roomUsers);
+  }
+});
+});
+app.get("/", (req, res) => {
+  res.send("server is up and running");
+});
+
+
+
 
 server.listen(PORT, () =>
   console.log(`server is listening on http://localhost:${PORT}`)
--- server/user.js
+++ server/user.js
@@ -3,33 +3,31 @@ const users = [];
 // Join user to chat
 const userJoin = (id, username, room, host, presenter) => {
   const user = { id, username, room, host, presenter };
-
   users.push(user);
   return user;
 };
+
 // User leaves chat
 const userLeave = (id) => {
   const index = users.findIndex((user) => user.id === id);
-
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 };
 
-//get users
-const getUsers = (room) => {
-  const RoomUsers = [];
-  users.map((user) => {
-    if (user.room == room) {
-      RoomUsers.push(user);
-    }
-  });
+// Get current user by socket id
+const getCurrentUser = (id) => {
+  return users.find((user) => user.id === id);
+};
 
-  return RoomUsers;
+// Get all users in a room
+const getUsers = (room) => {
+  return users.filter((user) => user.room === room);
 };
 
 module.exports = {
   userJoin,
   userLeave,
   getUsers,
+  getCurrentUser
 };
